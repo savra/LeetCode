c30a8fac5a9723f720479ac76297239bb776f7cb
Sergey Savrukhin <savra.sv@yandex.ru>
2022-11-10 09:54:06 +0300

    Range Sum of BST

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/RangeSumOfBST.java
@@ -0,0 +1,56 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Range Sum of BST",
+        url = "https://leetcode.com/problems/range-sum-of-bst/")
+public class RangeSumOfBST {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        int result = 0;
+
+        if (root.val >= low && root.val <= high) {
+            result += root.val;
+        }
+
+        return result + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(10);
+        treeNode.left = new TreeNode(5);
+        treeNode.right = new TreeNode(15);
+        treeNode.right.right = new TreeNode(18);
+        treeNode.left.left = new TreeNode(3);
+        treeNode.left.right = new TreeNode(7);
+
+        int sumBST = rangeSumBST(treeNode, 7, 15);
+
+        System.out.println(sumBST);
+    }
+}
