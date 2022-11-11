bebf9838c0542d413ffc23a0b8cda0bd4eded951
Sergey Savrukhin <savra.sv@yandex.ru>
2022-11-11 12:46:48 +0300

    Split a String in Balanced Strings

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/SplitAStringInBalancedStrings.java
@@ -0,0 +1,30 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Split a String in Balanced Strings",
+        url = "https://leetcode.com/problems/split-a-string-in-balanced-strings/description/")
+public class SplitAStringInBalancedStrings {
+    public int balancedStringSplit(String s) {
+        int lCount = 0;
+        int rCount = 0;
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                lCount++;
+            } else {
+                rCount++;
+            }
+
+            if (lCount == rCount) {
+                result++;
+                lCount = rCount = 0;
+            }
+        }
+
+        return result;
+    }
+}
