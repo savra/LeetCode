203aed2071e562115079772d01e31a448ae924b2
Sergey Savrukhin <savra.sv@yandex.ru>
2022-12-01 18:24:57 +0300

    Rearrange Products Table

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/RearrangeProductsTable.sql
@@ -0,0 +1,22 @@
+select *
+from (
+         SELECT product_id,
+                case when store1 is not null then 'store1' end as store,
+                store1                                         as price
+         from products
+
+         union
+
+         SELECT product_id,
+                case when store2 is not null then 'store2' end as store,
+                store2                                         as price
+         from products
+
+         union
+
+         SELECT product_id,
+                case when store3 is not null then 'store3' end as store,
+                store3                                         as price
+         from products)
+where price is not null
+order by store;
