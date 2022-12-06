fe195f89b98e7ff92585bd100a8545d64537898f
Sergey Savrukhin <savra.sv@yandex.ru>
2022-12-06 18:38:01 +0300

    Maximum 69 Number

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/Maximum69Number.java
@@ -0,0 +1,22 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Maximum 69 Number",
+        url = "https://leetcode.com/problems/maximum-69-number/")
+public class Maximum69Number {
+    public int maximum69Number (int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(new String(chars));
+    }
+}
