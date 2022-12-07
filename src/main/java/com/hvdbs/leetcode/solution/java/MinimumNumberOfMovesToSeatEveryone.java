208ec5a6344240654a9be6e47d603bc8551c2375
Sergey Savrukhin <savra.sv@yandex.ru>
2022-12-07 15:47:22 +0300

    Minimum Number of Moves to Seat Everyone

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,36 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Minimum Number of Moves to Seat Everyone",
+        url = "https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/")
+public class MinimumNumberOfMovesToSeatEveryone {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int movesCount = 0;
+
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] == students[i]) {
+                continue;
+            } else if (seats[i] < students[i]) {
+                while(seats[i] != students[i]) {
+                    movesCount++;
+                    seats[i]++;
+                }
+            } else {
+                while(seats[i] != students[i]) {
+                    movesCount++;
+                    seats[i]--;
+                }
+            }
+        }
+
+        return movesCount;
+    }
+}
