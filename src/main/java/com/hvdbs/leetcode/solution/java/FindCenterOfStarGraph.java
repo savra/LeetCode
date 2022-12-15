fdd706d4274ea560d751d688f8b496189b5d02d6
Sergey Savrukhin <savra.sv@yandex.ru>
2022-12-15 17:52:46 +0300

    Find Center of Star Graph

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/FindCenterOfStarGraph.java
@@ -0,0 +1,13 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Find Center of Star Graph",
+        url = "https://leetcode.com/problems/find-center-of-star-graph/description/")
+public class FindCenterOfStarGraph {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] ? edges[0][0] : edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
