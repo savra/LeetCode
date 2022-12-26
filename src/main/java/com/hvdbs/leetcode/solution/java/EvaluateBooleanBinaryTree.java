b2565f574edcc8c4300035c152cbf2adafef56b0
Sergey Savrukhin <savra.sv@yandex.ru>
2022-12-26 14:04:17 +0300

    Evaluate Boolean Binary Tree

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/EvaluateBooleanBinaryTree.java
@@ -0,0 +1,45 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Evaluate Boolean Binary Tree",
+        url = "https://leetcode.com/problems/evaluate-boolean-binary-tree/solutions/")
+public class EvaluateBooleanBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean evaluateTree(TreeNode root) {
+        return traverseTree(root);
+    }
+
+    private boolean traverseTree(TreeNode root) {
+        boolean result;
+
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+
+        boolean right = traverseTree(root.right);
+        boolean left = traverseTree(root.left);
+
+
+        if (root.val == 3) {
+            result = right && left;
+        } else {
+            result = right || left;
+        }
+
+        return result;
+    }
+}
