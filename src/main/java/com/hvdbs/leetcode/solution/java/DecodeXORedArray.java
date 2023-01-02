8a456e7e7241b7df48b87448f27656497e135eb2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-01-02 14:58:56 +0300

    Decode XORed Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/DecodeXORedArray.java
@@ -0,0 +1,21 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Decode XORed Array",
+        url = "https://leetcode.com/problems/decode-xored-array/description/")
+public class DecodeXORedArray {
+    public int[] decode(int[] encoded, int first) {
+        int[] result = new int[encoded.length + 1];
+
+        result[0] = first;
+
+        for (int i = 0; i < encoded.length; i++) {
+            result[i + 1] = result[i] ^ encoded[i];
+        }
+
+        return result;
+    }
+}
