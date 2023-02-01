683aea05a0129c57996bf64ae361abd64dc434a9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-01 17:54:53 +0300

    Reformat Department Table

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/ReformatDepartmentTable.sql
@@ -0,0 +1,15 @@
+SELECT id,
+       sum(CASE month WHEN 'Jan' THEN revenue END) AS Jan_Revenue,
+       sum(CASE month WHEN 'Feb' THEN revenue END) AS Feb_Revenue,
+       sum(CASE month WHEN 'Mar' THEN revenue END) AS Mar_Revenue,
+       sum(CASE month WHEN 'Apr' THEN revenue END) AS Apr_Revenue,
+       sum(CASE month WHEN 'May' THEN revenue END) AS May_Revenue,
+       sum(CASE month WHEN 'Jun' THEN revenue END) AS Jun_Revenue,
+       sum(CASE month WHEN 'Jul' THEN revenue END) AS Jul_Revenue,
+       sum(CASE month WHEN 'Aug' THEN revenue END) AS Aug_Revenue,
+       sum(CASE month WHEN 'Sep' THEN revenue END) AS Sep_Revenue,
+       sum(CASE month WHEN 'Oct' THEN revenue END) AS Oct_Revenue,
+       sum(CASE month WHEN 'Nov' THEN revenue END) AS Nov_Revenue,
+       sum(CASE month WHEN 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
