34590163012c6f9ed8c8fb3b3be61514b7976c43
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-03 18:18:57 +0300

    Sales Person

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/SalesPerson.sql
@@ -0,0 +1,8 @@
+SELECT sp.name
+FROM SalesPerson sp
+WHERE NOT EXISTS
+    (SELECT NULL
+     FROM orders o
+              JOIN company c ON (c.com_id = o.com_id)
+     WHERE c.name = 'RED'
+       AND sp.sales_id = o.sales_id)
