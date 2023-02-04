f80b1546b51bf43a8b0602012f28a61e7fb610e5
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-04 11:30:57 +0300

    Calculate Special Bonus

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/CalculateSpecialBonus.sql
@@ -0,0 +1,7 @@
+SELECT employee_id,
+       CASE
+           WHEN MOD(employee_id, 2) != 0 AND SUBSTR(name, 1, 1) != 'M' THEN
+               salary
+           ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id;
