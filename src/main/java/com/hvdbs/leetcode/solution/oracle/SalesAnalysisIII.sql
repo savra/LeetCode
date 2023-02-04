c7c470b3d390fa2e86dc02da61373318eeac69e1
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-04 20:48:28 +0300

    Classes More Than 5 Students

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/SalesAnalysisIII.sql
@@ -0,0 +1,4 @@
+SELECT p.product_id, p.product_name
+FROM product p
+         JOIN sales s ON (p.product_id = s.product_id)
+WHERE sale_date BETWEEN to_date('01.01.2019', 'dd.mm.yyyy') AND to_date('31.01.2019', 'dd.mm.yyyy')
