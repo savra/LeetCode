1e72019319e9c23b2c0b7acde453e00845934cc0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-05 09:55:46 +0300

    Tree Node

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/TreeNode.sql
@@ -0,0 +1,6 @@
+SELECT id,
+       CASE
+           WHEN p_id IS NULL THEN 'Root'
+           WHEN (select max(p_id) FROM tree t2 WHERE t2.p_id = t.id) IS NULL THEN 'Leaf'
+           ELSE 'Inner' END AS "type"
+FROM tree t;
