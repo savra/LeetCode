aac1ee511aeadbae9da0db13a59c7880316eeee0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-06 16:52:43 +0300

    Number of Strings That Appear as Substrings in Word

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,20 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Number of Strings That Appear as Substrings in Word",
+        url = "https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/description/")
+public class NumberOfStringsThatAppearAsSubstringsInWord {
+    public int numOfStrings(String[] patterns, String word) {
+        int result = 0;
+
+        for (int i = 0; i < patterns.length; i++) {
+            if (word.indexOf(patterns[i]) != -1) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
