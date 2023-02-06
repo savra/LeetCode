cd99da921f73c168cbe0209403ee5897daa1f4c0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-06 13:21:30 +0300

    Merge Nodes In Between Zeros

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MergeNodesInBetweenZeros.java
@@ -0,0 +1,57 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeNodesInBetweenZeros {
+      public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public static ListNode mergeNodes(ListNode head) {
+        int accumulator = 0;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (head != null) {
+            if (head.val == 0) {
+                if (accumulator != 0) {
+                    result.add(accumulator);
+                    accumulator = 0;
+                }
+            } else {
+                accumulator += head.val;
+            }
+
+            head = head.next;
+        }
+
+        ListNode root = new ListNode(result.get(0));
+
+        for (int i = 1; i < result.size(); i++) {
+            root.next = new ListNode(result.get(i));
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+          ListNode node = new ListNode(0);
+          node.next = new ListNode(1);
+          node.next.next = new ListNode(0);
+          node.next.next.next = new ListNode(3);
+          node.next.next.next.next = new ListNode(0);
+          node.next.next.next.next.next = new ListNode(2);
+          node.next.next.next.next.next.next = new ListNode(2);
+          node.next.next.next.next.next.next.next = new ListNode(0);
+
+        ListNode listNode = mergeNodes(node);
+    }
+}
