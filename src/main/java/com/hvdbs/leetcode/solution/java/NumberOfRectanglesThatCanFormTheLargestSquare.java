9f129b99fff91b802be09bfc64c44faa9ed98ce8
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-06 17:49:28 +0300

    Number Of Rectangles That Can Form The Largest Square

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/NumberOfRectanglesThatCanFormTheLargestSquare.java
@@ -0,0 +1,32 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Number Of Rectangles That Can Form The Largest Square",
+        url = "https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/")
+public class NumberOfRectanglesThatCanFormTheLargestSquare {
+    public int countGoodRectangles(int[][] rectangles) {
+        int largestSquares = 0;
+        int minLen = 0;
+        int[] result = new int[rectangles.length];
+
+        for (int i = 0; i < rectangles.length; i++) {
+            minLen = Math.min(rectangles[i][0], rectangles[i][1]);
+
+            if (minLen > largestSquares) {
+                largestSquares = minLen;
+            }
+
+            result[i] = minLen;
+        }
+
+        final int maximum = largestSquares;
+
+        return (int)Arrays.stream(result)
+                .filter(rectangleLen -> rectangleLen == maximum)
+                .count();
+    }
+}
