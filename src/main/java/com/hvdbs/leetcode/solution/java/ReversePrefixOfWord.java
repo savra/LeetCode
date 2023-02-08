fe04d3bdf0af5ef17c0d0adb3d859b6aefbfc5c7
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-08 12:49:13 +0300

    Reverse Prefix of Word

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/ReversePrefixOfWord.java
@@ -0,0 +1,21 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Reverse Prefix of Word",
+        url = "https://leetcode.com/problems/reverse-prefix-of-word/description/")
+public class ReversePrefixOfWord {
+    public String reversePrefix(String word, char ch) {
+        int chrPosition = word.indexOf(ch);
+
+        if (chrPosition != -1) {
+            StringBuilder sb = new StringBuilder(word.substring(0, chrPosition + 1));
+            String reversed = sb.reverse().toString();
+
+            return reversed + word.substring(chrPosition + 1);
+        } else {
+            return word;
+        }
+    }
+}
