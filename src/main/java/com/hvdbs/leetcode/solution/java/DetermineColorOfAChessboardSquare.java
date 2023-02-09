c9fbc4059b901faad15b89e4a541355da8b8939d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-09 11:56:27 +0300

    Determine Color of a Chessboard Square

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/DetermineColorOfAChessboardSquare.java
@@ -0,0 +1,17 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Determine Color of a Chessboard Square",
+        url = "https://leetcode.com/problems/determine-color-of-a-chessboard-square/")
+public class DetermineColorOfAChessboardSquare {
+    private static final int aCode = 63;
+
+    public boolean squareIsWhite(String coordinates) {
+        return (coordinates.charAt(0) - aCode) % 2 == 0
+                && Character.getNumericValue(coordinates.charAt(1)) % 2 != 0
+                || (coordinates.charAt(0) - aCode) % 2 != 0
+                && Character.getNumericValue(coordinates.charAt(1)) % 2 == 0 ? false : true;
+    }
+}
