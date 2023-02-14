150cb7d24590af6a114741f4b3772922580510fc
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-14 08:50:59 +0300

    Update AddTwoIntegers.java
    Co-authored-by: Sergey <savra.sv@mail.ru>

--- src/main/java/com/hvdbs/leetcode/solution/java/AddTwoIntegers.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/AddTwoIntegers.java
@@ -5,6 +5,7 @@ import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
 @LeetCodeInfo(difficulty = Difficulty.EASY, name = "Add Two Integers", url = "https://leetcode.com/problems/add-two-integers/")
 public class AddTwoIntegers {
+    
     public int sum(int num1, int num2) {
         return num1 + num2;
     }
