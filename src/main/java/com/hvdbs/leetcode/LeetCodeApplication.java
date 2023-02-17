2b6646d8be13b5149f5d7de4c19bc642ef548532
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-17 20:21:08 +0300

    DI String Match

--- src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
+++ src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
@@ -9,6 +9,7 @@ public class LeetCodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeetCodeApplication.class, args);
 
+
         StatsGenerator.generate();
     }
 }
