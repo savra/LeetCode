3cfe461b6aad3221006be6984d61dca8368d0946
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-20 10:57:52 +0300

    Check if All Characters Have Equal Number of Occurrences

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/ANumberAfterADoubleReversal.java
@@ -0,0 +1,31 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "A Number After a Double Reversal",
+        url = "https://leetcode.com/problems/a-number-after-a-double-reversal")
+public class ANumberAfterADoubleReversal {
+    public boolean isSameAfterReversals(int num) {
+        if (num == 0) {
+            return true;
+        }
+
+        int reversed = reverseNum(num);
+
+        return num == reverseNum(reversed);
+    }
+
+    private int reverseNum(int num) {
+        int tmp = num;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (tmp != 0) {
+            sb.append(tmp % 10);
+            tmp /= 10;
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+}
