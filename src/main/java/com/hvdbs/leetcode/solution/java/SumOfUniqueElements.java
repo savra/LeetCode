12dd57cd9e48926efed1d84e54677d9b514cba05
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-21 16:44:47 +0300

    Sum of Unique Elements

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/SumOfUniqueElements.java
@@ -0,0 +1,25 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Sum of Unique Elements",
+        url = "https://leetcode.com/problems/sum-of-unique-elements/")
+public class SumOfUniqueElements {
+    public int sumOfUnique(int[] nums) {
+        int[] result = new int[101];
+        int sumOfUnique = 0;
+
+        for (int num : nums) {
+            result[num]++;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == 1) {
+                sumOfUnique += i;
+            }
+        }
+
+        return sumOfUnique;
+    }
+}
