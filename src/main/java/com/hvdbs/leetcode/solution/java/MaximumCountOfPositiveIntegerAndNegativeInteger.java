26f54b3f1f6bb2534e4bc179a28434581ca4d687
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-21 16:30:48 +0300

    Maximum Count of Positive Integer and Negative Integer

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,22 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Maximum Count of Positive Integer and Negative Integer",
+        url = "https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/")
+public class MaximumCountOfPositiveIntegerAndNegativeInteger {
+    public int maximumCount(int[] nums) {
+        int maxNegative = 0, maxPositive = 0;
+
+        for (int num : nums) {
+            if (num < 0) {
+                maxNegative++;
+            } else if (num > 0) {
+                maxPositive++;
+            }
+        }
+
+        return Math.max(maxNegative, maxPositive);
+    }
+}
