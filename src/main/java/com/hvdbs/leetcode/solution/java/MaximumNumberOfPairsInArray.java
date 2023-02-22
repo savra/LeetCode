ceed6b2e0568f297dd4a1600e725ed8dc0a7d27d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-22 09:42:11 +0300

    Maximum Number of Pairs in Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MaximumNumberOfPairsInArray.java
@@ -0,0 +1,33 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Maximum Number of Pairs in Array",
+        url = "https://leetcode.com/problems/maximum-number-of-pairs-in-array/")
+public class MaximumNumberOfPairsInArray {
+    public int[] numberOfPairs(int[] nums) {
+        int[] result = new int[2];
+
+        if (nums.length == 1) {
+            result[1] = 1;
+
+            return result;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j <= nums.length - 1; j++) {
+                if (nums[i] >= 0 && nums[j] >= 0 && nums[i] == nums[j]) {
+                    nums[i] = nums[j] = -1;
+                    result[0]++;
+                }
+            }
+        }
+
+        result[1] = (int)Arrays.stream(nums).filter(i -> i >= 0).count();
+
+        return result;
+    }
+}
