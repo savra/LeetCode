4e832c50da38210cc604785e0b55a3d99a2fcee0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-02-28 17:40:31 +0300

    Sort Array By Parity

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/SortArrayByParity.java
@@ -0,0 +1,28 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Sort Array By Parity",
+        url = "https://leetcode.com/problems/sort-array-by-parity/")
+public class SortArrayByParity {
+    public int[] sortArrayByParity(int[] nums) {
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+
+        even.addAll(odd);
+
+        return even.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
