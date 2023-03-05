47c0427379da8ee8016afee32da0c2807a45fe79
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-05 09:28:44 +0300

    Divide Array Into Equal Pairs

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/DivideArrayIntoEqualPairs.java
@@ -0,0 +1,24 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Divide Array Into Equal Pairs",
+        url = "https://leetcode.com/problems/divide-array-into-equal-pairs/")
+public class DivideArrayIntoEqualPairs {
+    public boolean divideArray(int[] nums) {
+        Arrays.sort(nums);
+
+        int pairsCount = 0;
+
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] == nums[i + 1]) {
+                pairsCount++;
+            }
+        }
+
+        return pairsCount == nums.length / 2;
+    }
+}
