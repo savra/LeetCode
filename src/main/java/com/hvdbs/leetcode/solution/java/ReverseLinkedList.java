573f287a6fc5d8b5d663775b7421bd1ba4fd3cfe
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-14 08:57:14 +0300

    ReverseLinkedList

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.hvdbs.leetcode.solution.java;
+
+import java.util.*;
+
+public class ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        Deque<ListNode> stack = new ArrayDeque<>();
+
+        if (head == null) {
+
+        }
+
+        while (head.next != null) {
+
+        }
+    }
+}
