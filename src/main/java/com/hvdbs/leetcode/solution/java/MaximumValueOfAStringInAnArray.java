8de138e33b93e480ad7f5a378cca0ea648a2507d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-25 09:31:50 +0300

    Maximum Value of a String in an Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MaximumValueOfAStringInAnArray.java
@@ -0,0 +1,40 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Maximum Value of a String in an Array",
+        url = "https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/")
+public class MaximumValueOfAStringInAnArray {
+    public int maximumValue(String[] strs) {
+        int maxValue = 0;
+
+        for (int i = 0; i < strs.length; i++) {
+            try {
+                int tmp = Integer.parseInt(strs[i]);
+
+                if (tmp > maxValue) {
+                    maxValue = tmp;
+                }
+
+                strs[i] = Integer.toString(tmp);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+
+        Arrays.sort(strs, Comparator.comparingInt(String::length).reversed());
+
+        int result;
+
+        try {
+            result = Integer.parseInt(strs[0]);
+        } catch(NumberFormatException e) {
+            result = strs[0].length();
+        }
+
+        return Math.max(result, maxValue);
+    }
+}
