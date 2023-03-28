a448a7cd22fe48a4f6c227809ccda4ca8f6ea62d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-28 20:57:41 +0300

    Kth Distinct String in an Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/KthDistinctStringInAnArray.java
@@ -0,0 +1,26 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.*;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Kth Distinct String in an Array",
+        url = "https://leetcode.com/problems/kth-distinct-string-in-an-array")
+public class KthDistinctStringInAnArray {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> res = new LinkedHashMap<>();
+
+        for (String s : arr) {
+            res.put(s, res.getOrDefault(s, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> collect = res.entrySet()
+                .stream()
+                .filter(stringIntegerEntry -> stringIntegerEntry.getValue() == 1)
+                .toList();
+
+        return collect.size() < k ? "" : collect.get(k - 1).getKey();
+    }
+}
+
