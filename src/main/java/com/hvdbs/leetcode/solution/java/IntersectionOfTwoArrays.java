74b6ebce5a73d5f8a836e8f982d5407a2d57de53
Sergey Savrukhin <savra.sv@yandex.ru>
2023-04-17 20:07:31 +0300

    Intersection of Two Arrays

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/IntersectionOfTwoArrays.java
@@ -0,0 +1,19 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Intersection of Two Arrays",
+        url = "https://leetcode.com/problems/intersection-of-two-arrays/")
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {;
+        Set<Integer> first = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        first.retainAll(Arrays.stream(nums2).boxed().collect(Collectors.toSet()));
+
+        return first.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
