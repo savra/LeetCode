6c18e6b224e3ef55b96ba4b129c9d1b53bcaca6b
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-19 19:37:10 +0300

    Calculate Delayed Arrival Time

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/CalculateDelayedArrivalTime.java
@@ -0,0 +1,12 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Calculate Delayed Arrival Time",
+        url = "https://leetcode.com/problems/calculate-delayed-arrival-time")
+public class CalculateDelayedArrivalTime {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
