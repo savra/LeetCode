8c49dfa5c8344a8a2f009039ae164fc018dcc2a1
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-24 17:49:37 +0300

    Average Time of Process per Machine

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/AverageTimeOfProcessPerMachine.sql
@@ -0,0 +1,10 @@
+select machine_id, round(sum(end_time - start_time) / cnt, 3) as processing_time
+from (select machine_id,
+             count(process_id) over (partition by machine_id)           cnt,
+             max(case activity_type when 'start' then timestamp end) as start_time,
+             max(case activity_type when 'end' then timestamp end)   as end_time
+      from activity
+      group by machine_id,
+               PROCESS_ID)
+group by machine_id, cnt
+order by MACHINE_ID;
