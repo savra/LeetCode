849fa0ae75d8aa149ef5d420c9e209c3c49adc3a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-30 06:26:38 +0300

    Sort Array by Increasing Frequency

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/SortArrayByIncreasingFrequency.java
@@ -0,0 +1,26 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Sort Array by Increasing Frequency",
+        url = "https://leetcode.com/problems/sort-array-by-increasing-frequency")
+public class SortArrayByIncreasingFrequency {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Long> frequencyTable = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(v -> v, Collectors.counting()));
+
+        return IntStream.of(nums).boxed()
+                .sorted(Comparator.comparingLong(frequencyTable::get)
+                        .thenComparing((o1, o2) -> Integer.compare((int) o2, (int) o1)))
+                .mapToInt(value -> value)
+                .toArray();
+    }
+}
