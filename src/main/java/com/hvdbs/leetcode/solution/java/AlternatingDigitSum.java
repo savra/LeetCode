a37871d11ba8f35358b032011a3cdbee165293ca
Sergey Savrukhin <savra.sv@yandex.ru>
2023-06-02 16:47:50 +0300

    Alternating Digit Sum

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/AlternatingDigitSum.java
@@ -0,0 +1,33 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Alternating Digit Sum",
+        url = "https://leetcode.com/problems/alternating-digit-sum/")
+public class AlternatingDigitSum {
+    public int alternateDigitSum(int n) {
+        int multiplier = (int)Math.pow(10, (int)Math.log10(n));
+        int sum = 0;
+        int i = 2;
+
+        int tmp;
+        int rest = n;
+
+        while (multiplier != 0) {
+            tmp = rest / multiplier;
+            rest %= multiplier;
+
+            if (i % 2 != 0) {
+                sum += tmp * -1;
+            } else {
+                sum += tmp;
+            }
+
+            multiplier /= 10;
+            i++;
+        }
+
+        return sum;
+    }
+}
