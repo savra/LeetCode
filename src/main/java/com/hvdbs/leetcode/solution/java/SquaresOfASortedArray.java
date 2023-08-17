73030422cdfd99cc5e0023928fe0ba4e017f3cfd
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-17 18:17:17 +0300

    Squares of a Sorted Array

--- src/main/java/com/hvdbs/leetcode/solution/java/SquaresOfASortedArray.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/SquaresOfASortedArray.java
@@ -3,12 +3,25 @@ package com.hvdbs.leetcode.solution.java;
 import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
 import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
-import java.util.Arrays;
-
 @LeetCodeInfo(difficulty = Difficulty.EASY, name = "Squares of a Sorted Array",
         url = "https://leetcode.com/problems/squares-of-a-sorted-array/")
 public class SquaresOfASortedArray {
     public int[] sortedSquares(int[] nums) {
-        return Arrays.stream(nums).map(v -> v * v).sorted().toArray();
+        int resIndx = nums.length - 1;
+        int[] result = new int[nums.length];
+
+        int i = 0, j = nums.length - 1;
+
+        while (i <= j) {
+            if (Math.abs(nums[j]) >= Math.abs(nums[i])) {
+                result[resIndx--] = nums[j] * nums[j];
+                j--;
+            } else {
+                result[resIndx--] = nums[i] * nums[i];
+                i++;
+            }
+        }
+
+        return result;
     }
 }
