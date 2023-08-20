fa8a1097674eb63ec71d498d12ffe6bbeed4e42e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-20 12:09:32 +0300

    Count Pairs Whose Sum is Less than Target

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,29 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.List;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Count Pairs Whose Sum is Less than Target",
+        url = "https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target"
+)
+public class CountPairsWhoseSumIsLessThanTarget {
+    //My Time Complexity: O(n^2)
+    //My Space complexity: O(1)
+    public int countPairs(List<Integer> nums, int target) {
+        int result = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
