c25570f23f1cf1f68204828f284f7acd5b8d8e06
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-20 11:58:44 +0300

    Find the Maximum Achievable Number

--- src/main/java/com/hvdbs/leetcode/solution/java/FindTheMaximumAchievableNumber.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/FindTheMaximumAchievableNumber.java
@@ -9,6 +9,8 @@ import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
         url = "https://leetcode.com/problems/find-the-maximum-achievable-number"
 )
 public class FindTheMaximumAchievableNumber {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
     public int theMaximumAchievableX(int num, int t) {
         return num + (2 * t);
     }
