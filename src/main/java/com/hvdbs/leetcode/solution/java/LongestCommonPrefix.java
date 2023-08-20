58eaf00bb2171ec171292ef56b8847f51f31b7c2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-20 11:39:07 +0300

    Longest Common Prefix

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Longest Common Prefix",
+        url = "https://leetcode.com/problems/longest-common-prefix"
+)
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        int longestCount = 0, j = 0;
+
+        boolean wasIncrement = false;
+
+        while (j < strs[0].length()) {
+            for (int i = 1; i < strs.length; i++) {
+                wasIncrement = false;
+
+                if (j < strs[i].length()) {
+                    wasIncrement = true;
+                    if (strs[i].charAt(j) != strs[i - 1].charAt(j)) {
+                        return strs[0].substring(0, longestCount);
+                    }
+                } else {
+                    break;
+                }
+            }
+            j++;
+
+            if (wasIncrement) {
+                longestCount++;
+            }
+        }
+
+        return strs[0].substring(0, longestCount);
+    }
+}
