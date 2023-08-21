b966be7a25716bd78848bb2125ec2c285566b78c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-21 08:03:42 +0300

    Sum of Squares of Special Elements

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/SumOfSquaresOfSpecialElements.java
@@ -0,0 +1,26 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Sum of Squares of Special Elements",
+        url = "https://leetcode.com/problems/sum-of-squares-of-special-elements"
+)
+public class SumOfSquaresOfSpecialElements {
+    //My Time Complexity: O(n)
+    //My Space complexity: O(1)
+    public int sumOfSquares(int[] nums) {
+        int result = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (n % (i + 1) == 0) {
+                result += nums[i] * nums[i];
+            }
+        }
+
+        return result;
+    }
+}
