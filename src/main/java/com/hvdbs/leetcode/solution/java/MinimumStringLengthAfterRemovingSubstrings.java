e87b3e4c0e390b3f6f910e27b5ad3d6ac7ce8204
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-24 07:50:26 +0300

    Minimum String Length After Removing Substrings

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,25 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Minimum String Length After Removing Substrings",
+        url = "https://leetcode.com/problems/minimum-string-length-after-removing-substrings"
+)
+public class MinimumStringLengthAfterRemovingSubstrings {
+    public int minLength(String s) {
+        int prevSize = 0;
+        int currentSize = s.length();
+        String tmp = s;
+
+        while(currentSize != prevSize) {
+            prevSize = currentSize;
+            tmp = tmp.replaceAll("(AB)|(CD)", "");
+            currentSize = tmp.length();
+        }
+
+        return currentSize;
+    }
+}
