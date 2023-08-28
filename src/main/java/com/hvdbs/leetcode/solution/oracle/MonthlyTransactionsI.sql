0b29763e91179e71303a0c130d1a1dd54f2fb2da
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-28 18:30:33 +0300

    Monthly Transactions I

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/MonthlyTransactionsI.sql
@@ -0,0 +1,15 @@
+SELECT month,
+       country,
+       max(trans_count)                                         AS trans_count,
+       sum(CASE WHEN state = 'approved' THEN 1 ELSE 0 END)      AS approved_count,
+       max(trans_total_amt)                                     AS trans_total_amount,
+       sum(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM (SELECT country,
+             state,
+             amount,
+             to_char(trans_date, 'yyyy-mm')                                          AS month,
+             sum(amount) OVER (PARTITION BY country, to_char(trans_date, 'yyyy-mm')) AS trans_total_amt,
+             count(*) OVER (PARTITION BY country, to_char(trans_date, 'yyyy-mm'))    AS trans_count
+      FROM transactions)
+GROUP BY country, month
+ORDER BY month, country DESC;
