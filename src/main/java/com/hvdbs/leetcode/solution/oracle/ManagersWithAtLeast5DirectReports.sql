f3f42b394206f513dba5d349bf4b4d7e3454bc6e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-29 19:38:47 +0300

    Managers with at Least 5 Direct Reports

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,7 @@
+SELECT e.name
+FROM employee e
+WHERE id IN (SELECT manager.id
+             FROM employee emp
+                      JOIN employee manager ON (emp.managerId = manager.id)
+             GROUP BY manager.id
+             HAVING count(manager.name) >= 5);
