397df118fbda231a66063df5e37f28443598445f
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-31 19:05:29 +0300

    Market Analysis I

--- src/main/java/com/hvdbs/leetcode/solution/oracle/MarketAnalysisI.sql
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/MarketAnalysisI.sql
@@ -1,7 +1,7 @@
-select u.user_id                                    as buyer_id,
-       to_char(u.join_date, 'yyyy-mm-dd')           as join_date,
-       (select count(*)
-        from orders
-        where buyer_id = u.user_id
-          and extract(year from order_date) = 2019) as orders_in_2019
-from Users u
+SELECT u.user_id                                    AS buyer_id,
+       to_char(u.join_date, 'yyyy-mm-dd')           AS join_date,
+       (SELECT count(*)
+        FROM orders
+        WHERE buyer_id = u.user_id
+          AND extract(YEAR FROM order_date) = 2019) AS orders_in_2019
+FROM Users u
