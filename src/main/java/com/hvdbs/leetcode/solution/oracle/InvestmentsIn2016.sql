ed0966d16e64eb8cecb51555653e90365dd9d9eb
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-21 19:36:04 +0300

    Design Linked List

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/InvestmentsIn2016.sql
@@ -0,0 +1,4 @@
+SELECT round(sum(tiv_2016), 2) AS tiv_2016
+FROM insurance i1
+WHERE i1.tiv_2015 IN (SELECT tiv_2015 FROM insurance i2 WHERE i2.pid != i1.pid)
+  AND (i1.lat, i1.lon) NOT IN (SELECT i2.lat, i2.lon FROM insurance i2 WHERE i2.pid != i1.pid);
