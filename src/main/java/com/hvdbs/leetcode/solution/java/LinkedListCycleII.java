06823e4c207327386ec4d8053a10c098390bdf31
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-22 19:13:35 +0300

    Linked List Cycle II

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/LinkedListCycleII.java
@@ -0,0 +1,53 @@
+package com.hvdbs.leetcode.solution.java;
+
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Linked List Cycle II",
+        url = "https://leetcode.com/problems/linked-list-cycle-ii"
+)
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        if (fast != slow) {
+            return null;
+        }
+
+        slow = head;
+
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
