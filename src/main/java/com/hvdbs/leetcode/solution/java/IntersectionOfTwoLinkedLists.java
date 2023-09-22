752ee0357e82ba56dd49d231a8463f263f3a17e9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-22 20:06:26 +0300

    Intersection of Two Linked Lists

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,40 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Intersection of Two Linked Lists",
+        url = "https://leetcode.com/problems/intersection-of-two-linked-lists"
+)
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> res = new HashSet<>();
+
+        for (ListNode curA = headA; curA != null; curA = curA.next) {
+            res.add(curA);
+        }
+
+        for (ListNode curB = headB; curB != null; curB = curB.next) {
+            if (res.contains(curB)) {
+                return curB;
+            }
+        }
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
