d124982186d6a8f9fc5c2e68cacb0dd87845078a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-23 16:07:11 +0300

    Reverse Linked List

--- src/main/java/com/hvdbs/leetcode/solution/java/ReverseLinkedList.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/ReverseLinkedList.java
@@ -1,7 +1,14 @@
 package com.hvdbs.leetcode.solution.java;
 
-import java.util.*;
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Reverse Linked List",
+        url = "https://leetcode.com/problems/reverse-linked-list"
+)
 public class ReverseLinkedList {
     public static class ListNode {
         int val;
@@ -21,16 +28,23 @@ public class ReverseLinkedList {
     }
 
     public ListNode reverseList(ListNode head) {
-        Deque<ListNode> stack = new ArrayDeque<>();
-
         if (head == null) {
-
+            return null;
         }
 
-        while (head.next != null) {
-
+        if (head.next == null) {
+            return head;
         }
 
-        return new ListNode();
+        ListNode prev = null;
+
+        for(ListNode cur = head, next = null; cur != null;) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
     }
 }
