9acfc48c8918bacff8cb3055df99d5e80d6b3cd8
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-27 20:05:52 +0300

    Odd Even Linked List

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/OddEvenLinkedList.java
@@ -0,0 +1,62 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Odd Even Linked List",
+        url = "https://leetcode.com/problems/odd-even-linked-list"
+)
+public class OddEvenLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //My Time Complexity: O(n)
+    //My Space complexity: O(1)
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode even = head.next;
+        ListNode odd = head;
+        ListNode oddHead = odd;
+        ListNode evenHead = even;
+
+        while (true) {
+            if (odd.next.next != null) {
+                odd.next = odd.next.next;
+                odd = odd.next;
+            } else {
+                break;
+            }
+
+            if (even.next.next != null) {
+                even.next = even.next.next;
+                even = even.next;
+            } else {
+                break;
+            }
+        }
+
+        even.next = null;
+        odd.next = evenHead;
+
+        return oddHead;
+    }
+}
