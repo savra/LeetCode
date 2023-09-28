cc78c641e8c708680bbfdfee6b3c925eca853bd2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-28 19:36:44 +0300

    Plus One

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/PlusOne.java
@@ -0,0 +1,36 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Plus One",
+        url = "https://leetcode.com/problems/plus-one"
+)
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int memory = 0;
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+                memory++;
+            } else {
+                digits[i]++;
+                memory = 0;
+                break;
+            }
+        }
+
+        if (memory >= 1) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            System.arraycopy(digits, 0, res, 1, digits.length);
+
+            return res;
+        } else {
+            return digits;
+        }
+    }
+}
