c91fd08236ac20b97f331a4999f1df050410dc37
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-29 08:08:41 +0300

    Rotate List

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/RotateList.java
@@ -0,0 +1,52 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Rotate List",
+        url = "https://leetcode.com/problems/rotate-list"
+)
+public class RotateList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode cur = head;
+        int n = 1;
+
+        for (; cur.next != null; cur = cur.next) { n++; }
+        cur.next = head;
+
+        k = n - (k % n);
+
+        while (k > 0) {
+            cur = cur.next;
+            k--;
+        }
+
+        head = cur.next;
+        cur.next = null;
+
+        return head;
+    }
+}
