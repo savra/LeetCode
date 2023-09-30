82c3cd04132b975c78afaa87f9bb8d2d97e16657
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-30 20:03:09 +0300

    Diagonal Traverse

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/DiagonalTraverse.java
@@ -0,0 +1,48 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.*;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Diagonal Traverse",
+        url = "https://leetcode.com/problems/diagonal-traverse"
+)
+public class DiagonalTraverse {
+    public int[] findDiagonalOrder(int[][] mat) {
+        if (mat == null || mat.length == 0) {
+            return new int[0];
+        }
+
+        int rowsCount = mat.length;
+        int columnsCount = mat[0].length;
+
+        int[] res = new int[rowsCount * columnsCount];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < rowsCount; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                map.computeIfAbsent(i + j, k -> new ArrayList<>());
+                map.get(i + j).add(mat[i][j]);
+            }
+        }
+
+        int indx = 0;
+
+        for (int i = 0; i < rowsCount + columnsCount - 1; i++) {
+            List<Integer> tmp = map.get(i);
+
+            if (i % 2 == 0) {
+                Collections.reverse(tmp);
+            }
+
+            for (int item : tmp) {
+                res[indx++] = item;
+            }
+        }
+
+        return res;
+    }
+}
