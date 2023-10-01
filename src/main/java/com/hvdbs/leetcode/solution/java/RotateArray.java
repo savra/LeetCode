4ae5e9328c38261b19511ab5b7607e37f2633f12
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-01 19:06:39 +0300

    Rotate Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/RotateArray.java
@@ -0,0 +1,28 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Rotate Array",
+        url = "https://leetcode.com/problems/rotate-array"
+)
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
