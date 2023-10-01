4089eb48a5c59bb33f66a267fb315fd67af8f28b
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-01 17:26:58 +0300

    Minimum Size Subarray Sum

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MinimumSizeSubarraySum.java
@@ -0,0 +1,53 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Minimum Size Subarray Sum",
+        url = "https://leetcode.com/problems/minimum-size-subarray-sum"
+)
+public class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLength = Integer.MAX_VALUE;
+
+        if (nums.length == 1) {
+            if (target <= nums[0]) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        int curSum = nums[0];
+
+        if (curSum >= target) {
+            return 1;
+        }
+
+        boolean addTailSum = true;
+
+        for (int i = 0, j = 1; j <= nums.length - 1;) {
+            if (addTailSum) {
+                curSum += nums[j];
+            }
+
+            if (curSum < target) {
+                j++;
+                addTailSum = true;
+            } else {
+                if (j - i + 1 <= minLength) {
+                    minLength = j - i + 1;
+                }
+
+                curSum -= nums[i];
+                addTailSum = false;
+
+                i++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
