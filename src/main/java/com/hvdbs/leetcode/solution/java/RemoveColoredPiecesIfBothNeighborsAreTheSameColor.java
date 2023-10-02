5c4c8d05bf2b9cd09610d4f7b1d83929d321f792
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-02 08:37:17 +0300

    Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/RemoveColoredPiecesIfBothNeighborsAreTheSameColor.java
@@ -0,0 +1,28 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Remove Colored Pieces if Both Neighbors are the Same Color",
+        url = "https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/"
+)
+public class RemoveColoredPiecesIfBothNeighborsAreTheSameColor {
+    public boolean winnerOfGame(String colors) {
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 1; i < colors.length() - 1; i++) {
+            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
+                if (colors.charAt(i) == 'A') {
+                    aliceScore++;
+                } else {
+                    bobScore++;
+                }
+            }
+        }
+
+        return aliceScore > bobScore;
+    }
+}
