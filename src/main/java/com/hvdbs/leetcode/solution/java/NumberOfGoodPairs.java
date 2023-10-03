6dc1c93b571f93fc00b53f7ab932c56458cc6fdf
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-03 07:54:05 +0300

    Number of Good Pairs

--- src/main/java/com/hvdbs/leetcode/solution/java/NumberOfGoodPairs.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/NumberOfGoodPairs.java
@@ -3,19 +3,55 @@ package com.hvdbs.leetcode.solution.java;
 import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
 import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
-@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Number of Good Pairs", url = "https://leetcode.com/problems/number-of-good-pairs/")
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Number of Good Pairs",
+        url = "https://leetcode.com/problems/number-of-good-pairs/")
 public class NumberOfGoodPairs {
-    public int numIdenticalPairs(int[] nums) {
-        int goodPairs = 0;
+    /*Time complexity: O(n^2), Space complexity: O(1)*/
+    /*public int numIdenticalPairs(int[] nums) {
+        int res = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j] && i < j) {
-                    goodPairs++;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    res++;
                 }
             }
         }
 
-        return goodPairs;
+        return res;
+    }*/
+
+    /*Time complexity: O(n), Space complexity: O(n)*/
+    public int numIdenticalPairs(int[] nums) {
+        int[] freq = new int[101];
+
+        int res = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int num : freq) {
+            res += num * (num - 1) / 2;
+        }
+
+        return res;
     }
+
+    /*Time complexity: O(n), Space complexity: O(n)*/
+   /* public int numIdenticalPairs(int[] nums) {
+        int res = 0;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num: nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for(Integer e: freqMap.values()) {
+            res += e * (e - 1) / 2;
+        }
+
+        return res;
+    }*/
 }
