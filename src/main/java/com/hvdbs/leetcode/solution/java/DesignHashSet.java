98faeacff8a514f131f1cba12261db403e72e3f9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-04 08:22:52 +0300

    Design HashSet

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/DesignHashSet.java
@@ -0,0 +1,59 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Design HashSet",
+        url = "https://leetcode.com/problems/design-hashset/solutions/1969506/simple-java-implementation-rehashing-load-factor-80-faster/"
+)
+public class DesignHashSet {
+    class MyHashSet {
+        private static final int SIZE = 16;
+        private final List<LinkedList<Integer>> buckets = new ArrayList<>(SIZE);
+
+        public MyHashSet() {
+            for(int i = 0; i < SIZE; i++) {
+                buckets.add(new LinkedList<>());
+            }
+        }
+
+        public void add(int key) {
+            int i = buckets.size() % 16;
+
+            if (buckets.get(i) == null) {
+                buckets.add(i, new LinkedList<>());
+            }
+
+            if (!buckets.get(i).contains(key)) {
+                buckets.get(i).offerLast(key);
+            }
+        }
+
+        public void remove(int key) {
+            int i = buckets.size() % 16;
+
+            if (buckets.get(i) == null) {
+                return;
+            }
+
+            LinkedList<Integer> integers = buckets.get(i);
+            integers.removeFirstOccurrence(key);
+        }
+
+        public boolean contains(int key) {
+            int i = buckets.size() % 16;
+
+            if (buckets.get(i) == null) {
+                return false;
+            }
+
+            return buckets.get(i).contains(key);
+        }
+    }
+}
