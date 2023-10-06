e314a1fe15c66424b7925dff188713afe039685f
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-06 20:07:31 +0300

    Integer Break

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/IntegerBreak.java
@@ -0,0 +1,26 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Integer Break",
+        url = "https://leetcode.com/problems/integer-break"
+)
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+
+        int result = 1;
+
+        while (n > 4) {
+            result *= 3;
+            n -= 3;
+        }
+
+        return result * n;
+    }
+}
