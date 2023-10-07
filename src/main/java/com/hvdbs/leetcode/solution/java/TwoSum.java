0d682e6fed9232ccd4c9d508e26cab49f4683faa
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-07 14:02:57 +0300

    Two Sum

--- src/main/java/com/hvdbs/leetcode/solution/java/TwoSum.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/TwoSum.java
@@ -3,22 +3,42 @@ package com.hvdbs.leetcode.solution.java;
 import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
 import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
-@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Two Sum", url = "https://leetcode.com/problems/two-sum/")
+import java.util.HashMap;
+import java.util.Map;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Two Sum",
+        url = "https://leetcode.com/problems/two-sum/")
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        //TC: O(N), SC: O(1)
+       /* int[] result = new int[2];
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-
-                    break;
+                    return new int[] {i, j};
                 }
             }
         }
 
-        return result;
+        return result;*/
+
+        //TC: O(N), SC: O(N)
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+
+            if (map.containsKey(tmp) && map.get(tmp) != i) {
+                return new int[]{i, map.get(tmp)};
+            }
+        }
+
+        return null;
     }
 }
