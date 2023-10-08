ff727e50780b1c6d8a0d360599ea789c36f36f99
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-08 17:51:50 +0300

    Find the Difference

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/FindTheDifference.java
@@ -0,0 +1,56 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Find the Difference",
+        url = "https://leetcode.com/problems/find-the-difference"
+)
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        //TC: O(N + M), SC: O(N + M)
+       /* if (s.length() == 0) {
+            return t.charAt(0);
+        }
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> e : map2.entrySet()) {
+            if (!e.getValue().equals(map1.get(e.getKey()))) {
+                return e.getKey();
+            }
+        }
+
+        return s.charAt(0);*/
+
+        //TC: O(N), SC: O(1)
+        if (s.length() == 0) {
+            return t.charAt(0);
+        }
+
+        int c = 0;
+
+        for(char cs : s.toCharArray()) {
+            c ^= cs;
+        }
+
+        for (char ct : t.toCharArray()) {
+            c ^= ct;
+        }
+
+        return (char)c;
+    }
+}
