e32a40042067beffbf8d0450aa2bd017b58fa7c9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-10 20:06:09 +0300

    Valid Anagram

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/ValidAnagram.java
@@ -0,0 +1,34 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Valid Anagram",
+        url = "https://leetcode.com/problems/valid-anagram"
+)
+public class ValidAnagram {
+    //TC: O(N * LogN), SC: O(N + M)
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        for (int i = 0; i < c1.length; i++) {
+            if (c1[i] != c2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
