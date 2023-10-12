0c231b4dbf79a040ef7ab954154a9fedf38f5d33
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-12 18:08:45 +0300

    Repeated Substring Pattern

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/RepeatedSubstringPattern.java
@@ -0,0 +1,37 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Repeated Substring Pattern",
+        url = "https://leetcode.com/problems/repeated-substring-pattern"
+)
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        if (s.length() == 1) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= s.length() / 2; i++) {
+            if (s.length() % i == 0) {
+                String str = s.substring(0, i);
+
+                for (int j = 0; j < s.length(); j += str.length()) {
+                    sb.append(str);
+                }
+
+                if (sb.toString().equals(s)) {
+                    return true;
+                } else {
+                    sb.setLength(0);
+                }
+            }
+        }
+
+        return false;
+    }
+}
