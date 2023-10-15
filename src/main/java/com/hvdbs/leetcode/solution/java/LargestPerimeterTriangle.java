5413570a2763770a2519761f736c47fb36b62ced
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-15 18:15:53 +0300

    Largest Perimeter Triangle

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/LargestPerimeterTriangle.java
@@ -0,0 +1,24 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Largest Perimeter Triangle",
+        url = "https://leetcode.com/problems/largest-perimeter-triangle")
+public class LargestPerimeterTriangle {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = nums.length - 3; i >= 0; i--) {
+            if (nums[i] + nums[i + 1] > nums[i + 2]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return 0;
+    }
+}
