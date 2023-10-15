559c37fef3e6b700ad068725d5a08f072c61c1a0
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-15 18:06:07 +0300

    Lemonade Change

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/LemonadeChange.java
@@ -0,0 +1,56 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Lemonade Change",
+        url = "https://leetcode.com/problems/lemonade-change"
+)
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int[] wallet = new int[2];
+        int change = 0;
+
+        for (int bill : bills) {
+            change = 0;
+
+            if (bill == 5) {
+                wallet[0]++;
+            } else if (bill == 10) {
+                if (wallet[0] > 0) {
+                    wallet[0]--;
+                } else {
+                    return false;
+                }
+
+                wallet[1]++;
+            } else {
+                if (wallet[1] > 0) {
+                    wallet[1]--;
+                    change += 10;
+                }
+
+                if (wallet[0] > 0) {
+                    if (change != 0) {
+                        wallet[0]--;
+                    } else {
+                        while (change != 15 && wallet[0] > 0) {
+                            change += 5;
+                            wallet[0]--;
+                        }
+
+                        if (change != 15) {
+                            return false;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
