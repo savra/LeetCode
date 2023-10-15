20d76add3869749f778f65086010a15320804dac
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-15 09:45:17 +0300

    Monotonic Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MonotonicArray.java
@@ -0,0 +1,36 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Monotonic Array",
+        url = "https://leetcode.com/problems/monotonic-array"
+)
+public class MonotonicArray {
+    public boolean isMonotonic(int[] nums) {
+        if (nums.length <= 2) {
+            return true;
+        }
+
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int i = 0, j = 1; j < nums.length; i++, j++) {
+            if (nums[i] > nums[j]) {
+                isIncreasing = false;
+                break;
+            }
+        }
+
+        for (int i = 0, j = 1; j < nums.length; i++, j++) {
+            if (nums[j] > nums[i]) {
+                isDecreasing = false;
+                break;
+            }
+        }
+
+        return isIncreasing | isDecreasing;
+    }
+}
