b9a64a9628f6665c53958bc31b160f5a9637b3b8
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-17 19:58:05 +0300

    First Unique Character in a String

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/FirstUniqueCharacterInAString.java
@@ -0,0 +1,34 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "First Unique Character in a String",
+        url = "https://leetcode.com/problems/first-unique-character-in-a-string"
+)
+public class FirstUniqueCharacterInAString {
+    //TC: O(N), SC: O(N)
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            m.put(c, m.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (m.get(c) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
