afb83bc208d2586c27228b187f5bb31087df2f54
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-23 18:25:30 +0300

    Evaluate Reverse Polish Notation

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/EvaluateReversePolishNotation.java
@@ -0,0 +1,40 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Evaluate Reverse Polish Notation",
+        url = "https://leetcode.com/problems/evaluate-reverse-polish-notation"
+)
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] t) {
+        Deque<String> s = new ArrayDeque<>();
+
+        for (String value : t) {
+            if ("+".equals(value) || "-".equals(value) || "*".equals(value) || "/".equals(value)) {
+                s.offerFirst(value);
+                String oper = s.pollFirst();
+                int o2 = Integer.parseInt(s.pollFirst());
+                int o1 = Integer.parseInt(s.pollFirst());
+
+                int result = switch (oper) {
+                    case "+" -> o1 + o2;
+                    case "-" -> o1 - o2;
+                    case "*" -> o1 * o2;
+                    default -> o1 / o2;
+                };
+
+                s.offerFirst(String.valueOf(result));
+            } else {
+                s.offerFirst(value);
+            }
+        }
+
+        return Integer.parseInt(s.pollFirst());
+    }
+}
