d7f2fe58c5200fdf1157ee2a72dc93ae933c12e7
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-26 15:54:23 +0300

    Reverse String

--- src/main/java/com/hvdbs/leetcode/solution/java/ReverseString.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/ReverseString.java
@@ -7,14 +7,26 @@ import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
         url = "https://leetcode.com/problems/reverse-string")
 public class ReverseString {
     public void reverseString(char[] s) {
-        int middleElem = s.length / 2;
+        reverse(0, s.length - 1, s);
 
-        char tmp;
+        /*char tmp;
 
-        for(int i = 0, j = s.length - 1; i < middleElem && j >= middleElem; i++, j--) {
+        for(int i = 0, j = s.length - 1; i < j; i++, j--) {
             tmp = s[i];
             s[i] = s[j];
             s[j] = tmp;
+        }*/
+    }
+
+    private void reverse(int i, int j, char[] source) {
+        if (i >= j) {
+            return;
         }
+
+        char tmp = source[i];
+        source[i] = source[j];
+        source[j] = tmp;
+
+        reverse(i + 1, j - 1, source);
     }
 }
