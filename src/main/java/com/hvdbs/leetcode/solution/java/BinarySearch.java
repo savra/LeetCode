a1db68c9d230704f464928a95124727d0a6f6662
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-27 18:47:24 +0300

    Binary Search

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/BinarySearch.java
@@ -0,0 +1,30 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Binary Search",
+        url = "https://leetcode.com/problems/binary-search")
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        int m;
+
+        while (l <= h) {
+            m = l + (h - l) / 2;
+
+            if (target == nums[m]) {
+                return m;
+            } else if (target < nums[m]) {
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+}
