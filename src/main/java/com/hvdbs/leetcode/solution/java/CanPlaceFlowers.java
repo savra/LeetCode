1649bafc176ba1aeb00add19212f18b8384f0230
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-27 18:16:42 +0300

    Can Place Flowers

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/CanPlaceFlowers.java
@@ -0,0 +1,38 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Can Place Flowers",
+        url = "https://leetcode.com/problems/can-place-flowers")
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] f, int n) {
+        if (f.length == 1 && f[0] == 0) {
+            return true;
+        }
+
+        if (f.length == 2 && f[0] == 0 && f[1] == 0 && n != 2) {
+            return true;
+        }
+
+        if (f.length > 2 && f[0] == 0 && f[1] == 0) {
+            f[0] = 1;
+            n--;
+        }
+
+        for (int i = 1; i < f.length - 1; i++) {
+            if (f[i - 1] == 0 && f[i] == 0 && f[i + 1] == 0) {
+                f[i] = 1;
+                n--;
+            }
+        }
+
+        if (f.length > 2 && f[f.length - 2] == 0 && f[f.length - 1] == 0) {
+            n--;
+        }
+
+        return n <= 0;
+    }
+}
