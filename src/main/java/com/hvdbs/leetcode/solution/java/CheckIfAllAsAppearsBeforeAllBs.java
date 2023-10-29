62e2619602d7c21509614f6fb080d720fdff959a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-29 20:47:02 +0300

    Count Square Sum Triples

--- src/main/java/com/hvdbs/leetcode/solution/java/CheckIfAllAsAppearsBeforeAllBs.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/CheckIfAllAsAppearsBeforeAllBs.java
@@ -3,7 +3,8 @@ package com.hvdbs.leetcode.solution.java;
 import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
 import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
 
-@LeetCodeInfo(difficulty = Difficulty.EASY, name = "Check if All A's Appears Before All B's",
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Check if All A's Appears Before All B's",
         url = "https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/")
 public class CheckIfAllAsAppearsBeforeAllBs {
     public boolean checkString(String s) {
