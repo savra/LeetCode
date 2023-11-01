e887373bec137db66a20a08e1d05a9cdb4896ab5
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-01 07:52:16 +0300

    Binary Tree Level Order Traversal

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,68 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "Binary Tree Level Order Traversal",
+        url = "https://leetcode.com/problems/binary-tree-level-order-traversal")
+public class BinaryTreeLevelOrderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = q.poll();
+
+                currLevel.add(currNode.val);
+
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+
+            res.add(currLevel);
+        }
+
+        return res;
+    }
+}
