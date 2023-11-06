0a3187553f948155ddbf04af16610151e8131c78
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-06 15:48:01 +0300

    Find the Distance Value Between Two Arrays

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/FindTheDistanceValueBetweenTwoArrays.java
@@ -0,0 +1,32 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Find the Distance Value Between Two Arrays",
+        url = "https://leetcode.com/problems/find-the-distance-value-between-two-arrays")
+public class FindTheDistanceValueBetweenTwoArrays {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int res = 0;
+        boolean wasError;
+
+        for (int i = 0; i < arr1.length; i++) {
+            wasError = false;
+
+            for (int j = 0; j < arr2.length; j++) {
+                if (Math.abs(arr1[i] - arr2[j]) <= d) {
+                    wasError = true;
+                    break;
+                }
+            }
+
+            if (!wasError) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
