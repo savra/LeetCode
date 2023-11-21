4c37651f29d2655e072c0211428e7926893809da
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-21 21:33:52 +0300

    N-ary Tree Level Order Traversal

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/NaryTreeLevelOrderTraversal.java
@@ -0,0 +1,62 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.MEDIUM,
+        name = "N-ary Tree Level Order Traversal",
+        url = "https://leetcode.com/problems/n-ary-tree-level-order-traversal")
+public class NaryTreeLevelOrderTraversal {
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<Node> q = new ArrayDeque<>();
+
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> curLevel = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                Node cur = q.poll();
+
+                for (Node n : cur.children) {
+                    q.offer(n);
+                }
+
+                curLevel.add(cur.val);
+            }
+
+            res.add(curLevel);
+        }
+
+        return res;
+    }
+}
