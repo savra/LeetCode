a9abdc06702b831e42dcd3dc9b49a1c929145e37
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-26 14:18:57 +0300

    Confirmation Rate

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/ConfirmationRate.sql
@@ -0,0 +1,5 @@
+select user_id, round(actions / total, 2) as confirmation_rate
+from (select s.USER_ID, sum(case when ACTION = 'confirmed' THEN 1 else 0 end) as actions, count(*) as total
+      from Signups s
+               left join Confirmations c on (s.user_id = c.user_id)
+      group by s.USER_ID);
