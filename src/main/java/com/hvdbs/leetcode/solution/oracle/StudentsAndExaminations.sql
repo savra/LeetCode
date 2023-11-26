b9d1198958465e32ed321d3dc1b259e55f27258d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-26 13:42:52 +0300

    Students and Examinations

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/StudentsAndExaminations.sql
@@ -0,0 +1,6 @@
+select s.STUDENT_ID, s.student_name, sub.subject_name, count(e.student_id) as attended_exams
+from Students s
+         cross join Subjects sub
+         left join Examinations e on (s.STUDENT_ID = e.STUDENT_ID AND e.subject_name = sub.subject_name)
+GROUP BY s.STUDENT_ID, s.student_name, sub.subject_name
+order by s.STUDENT_ID;
