36ccf459cb272b0bf6d1b7c79d5520dc0f895770
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-27 08:00:15 +0300

    Biggest Single Number

---
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/BiggestSingleNumber.sql
@@ -0,0 +1,9 @@
+SELECT num
+FROM ((SELECT num, COUNT(*) cnt
+       FROM MyNumbers
+       GROUP BY num
+       HAVING COUNT(*) = 1)
+      UNION ALL
+      (SELECT null AS num, 1 cnt FROM dual)
+      ORDER BY num DESC NULLS LAST)
+WHERE rownum = 1;
