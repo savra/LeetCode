3503df5cfc65423c48cb32393e6163079441b6cd
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-29 20:33:03 +0300

    Number of Lines To Write String

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/NumberOfLinesToWriteString.java
@@ -0,0 +1,28 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Number of Lines To Write String",
+        url = "https://leetcode.com/problems/number-of-lines-to-write-string")
+public class NumberOfLinesToWriteString {
+    public int[] numberOfLines(int[] widths, String s) {
+        int width = 0;
+        int rowCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int pos = s.charAt(i) - 'a';
+
+            if (width + widths[pos] <= 100) {
+                width += widths[pos];
+            } else {
+                width = widths[pos];
+                rowCount++;
+            }
+        }
+
+        return new int[]{rowCount + 1, width};
+    }
+}
