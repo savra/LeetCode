4e4a372d62e3228a4d3f93c7fa1b924f9b002ed3
Sergey Savrukhin <savra.sv@yandex.ru>
2023-12-13 18:54:00 +0300

    Number of Lines To Write String

--- src/main/java/com/hvdbs/leetcode/solution/oracle/QueriesQualityAndPercentage.sql
+++ src/main/java/com/hvdbs/leetcode/solution/oracle/QueriesQualityAndPercentage.sql
@@ -1,5 +1,5 @@
 SELECT query_name,
-       round(summ / cnt, 2) AS                                     quality,
+       round(summ / cnt, 2) AS                                    quality,
        round(1 / (SELECT count(*) FROM Queries WHERE rating <= 3) * 100, 2) poor_query_percentage
 FROM (SELECT query_name, sum(rating / position) AS summ, count(*) AS cnt
       FROM Queries
