5ab86cf6dbc38700760c88a0aeedf8b746a4f838
Sergey Savrukhin <savra.sv@yandex.ru>
2024-01-19 20:03:10 +0300

    Apply Operations to an Array

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/ApplyOperationsToAnArray.java
@@ -0,0 +1,38 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Apply Operations to an Array",
+        url = "https://leetcode.com/problems/apply-operations-to-an-array"
+)
+public class ApplyOperationsToAnArray {
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+                i++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] != 0) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                    break;
+                }
+            }
+        }
+
+        return nums;
+    }
+}
