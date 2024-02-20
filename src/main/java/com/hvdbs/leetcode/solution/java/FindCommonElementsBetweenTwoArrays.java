afb883931d0e8896288585c1b073243a9ec5078c
Sergey Savrukhin <savra.sv@yandex.ru>
2024-02-20 19:01:28 +0300

    Find Common Elements Between Two Arrays

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/FindCommonElementsBetweenTwoArrays.java
@@ -0,0 +1,35 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Find Common Elements Between Two Arrays",
+        url = "https://leetcode.com/problems/find-common-elements-between-two-arrays")
+public class FindCommonElementsBetweenTwoArrays {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int[] result = new int[2];
+
+        result[0] = countCommonElements(nums1, nums2);
+        result[1] = countCommonElements(nums2, nums1);
+
+        return result;
+    }
+
+
+    private int countCommonElements(int[] nums1, int[] nums2) {
+        int result = 0;
+
+        for (int j : nums1) {
+            for (int k : nums2) {
+                if (j == k) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
