60d4f39563eccfd7c8b6860879bbe736a1866843
Sergey Savrukhin <savra.sv@yandex.ru>
2024-02-25 14:34:25 +0300

    Maximum Strong Pair XOR I

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/MaximumStrongPairXORI.java
@@ -0,0 +1,24 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Maximum Strong Pair XOR I",
+        url = "https://leetcode.com/problems/maximum-strong-pair-xor-i")
+public class MaximumStrongPairXORI {
+    public int maximumStrongPairXor(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (Math.abs(nums[j] - nums[i]) <= Math.min(nums[i], nums[j]) && ((nums[i] ^ nums[j]) > result)) {
+                    result = nums[i] ^ nums[j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
