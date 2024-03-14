4e8ba479662dff6a64e6e38792aa04bc1227ffca
Sergey Savrukhin <savra.sv@yandex.ru>
2024-03-14 20:24:51 +0300

    Ant on the Boundary

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/AntOnTheBoundary.java
@@ -0,0 +1,24 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Ant on the Boundary",
+        url = "https://leetcode.com/problems/ant-on-the-boundary")
+public class AntOnTheBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int antPosition = 0;
+        int result = 0;
+
+        for (int num : nums) {
+            antPosition += num;
+
+            if (antPosition == 0) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
