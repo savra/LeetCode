e3f91008466dd96faeaa309c3eac05a601421cac
Sergey Savrukhin <savra.sv@yandex.ru>
2024-03-24 19:58:23 +0300

    Largest 3-Same-Digit Number in String

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/Largest3SameDigitNumberInString.java
@@ -0,0 +1,27 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.leetcode.statsgenerator.LeetCodeInfo;
+import com.hvdbs.leetcode.statsgenerator.enums.Difficulty;
+
+@LeetCodeInfo(difficulty = Difficulty.EASY,
+        name = "Largest 3-Same-Digit Number in String",
+        url = "https://leetcode.com/problems/largest-3-same-digit-number-in-string")
+public class Largest3SameDigitNumberInString {
+    public String largestGoodInteger(String num) {
+        int max = -1;
+
+        if (num == null || num.length() < 3) {
+            return "";
+        }
+
+        for (int i = 2; i < num.length(); i++) {
+            if (num.charAt(i - 2) == num.charAt(i - 1) && num.charAt(i - 1) == num.charAt(i)) {
+                if (Character.getNumericValue(num.charAt(i)) > max) {
+                    max = Character.getNumericValue(num.charAt(i));
+                }
+            }
+        }
+
+        return max > -1 ? String.valueOf(max).repeat(3) : "";
+    }
+}
