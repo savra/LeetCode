c4a13e407d69a07ee04f6d17d9cb485c1436404c
Sergey Savrukhin <savra.sv@yandex.ru>
2024-05-07 21:46:22 +0300

    replace stats generator on  spring-boot-starter

--- src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
+++ src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeetCodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeetCodeApplication.class, args);
-
         StatisticsGenerator.generate();
     }
 }
