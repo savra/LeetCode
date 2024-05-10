97dcd25ced2b8574d5da5ab4c9920ccbdad1305f
Sergey Savrukhin <savra.sv@yandex.ru>
2024-05-10 06:37:19 +0300

    Настроил работу с statistics-generator-starter

--- src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
+++ src/main/java/com/hvdbs/leetcode/LeetCodeApplication.java
@@ -1,13 +1,23 @@
 package com.hvdbs.leetcode;
 
 import com.hvdbs.savra.statsgenerator.StatisticsGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class LeetCodeApplication {
+    private final StatisticsGenerator statisticsGenerator;
+
     public static void main(String[] args) {
         SpringApplication.run(LeetCodeApplication.class, args);
-        StatisticsGenerator.generate();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void startup() {
+        statisticsGenerator.generate();
     }
 }
