c8f37f3436c93460919217ec49be03247c6ae1ab
Sergey Savrukhin <savra.sv@yandex.ru>
2024-11-16 18:33:10 +0300

    Count Complete Tree Nodes

---
+++ src/main/java/com/hvdbs/leetcode/solution/java/CountCompleteTreeNodes.java
@@ -0,0 +1,40 @@
+package com.hvdbs.leetcode.solution.java;
+
+import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
+import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+@CodeInfo(
+        difficulty = Difficulty.EASY,
+        name = "Count Complete Tree Nodes",
+        url = "https://leetcode.com/problems/count-complete-tree-nodes",
+        spaceComplexity = Complexity.ConstantComplexity.LINEAR,
+        timeComplexity = Complexity.ConstantComplexity.LINEAR)
+public class CountCompleteTreeNodes {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
