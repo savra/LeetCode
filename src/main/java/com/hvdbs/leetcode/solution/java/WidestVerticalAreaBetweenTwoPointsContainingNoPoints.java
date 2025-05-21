84ce31b56cd8589e742af8379df5fb28feaa6006
Sergey Savrukhin <savra.sv@yandex.ru>
2025-05-21 21:39:29 +0300

    Widest Vertical Area Between Two Points Containing No Points

--- src/main/java/com/hvdbs/leetcode/solution/java/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -1,5 +1,20 @@
 package com.hvdbs.leetcode.solution.java;
 
-public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
+import java.util.Arrays;
+import java.util.Comparator;
 
+public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (o1, o2) -> {
+            if (o1[0] > o2[0]) {
+                return 1;
+            } else if (o1[0] < o2[0]) {
+                return -1;
+            } else {
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
+
+        for ()
+    }
 }
