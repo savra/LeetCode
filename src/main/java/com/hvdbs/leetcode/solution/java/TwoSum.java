cb7fb90bb6526843ed228724341d9fe0f3fa1711
Sergey Savrukhin <savra.sv@yandex.ru>
2026-01-31 21:14:18 +0300

    Two Sum

--- src/main/java/com/hvdbs/leetcode/solution/java/TwoSum.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/TwoSum.java
@@ -10,35 +10,19 @@ import java.util.Map;
         name = "Two Sum",
         url = "https://leetcode.com/problems/two-sum/",
         timeComplexity = "O(N)",
-        spaceComplexity = "O(1)")
+        spaceComplexity = "O(N)")
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        //TC: O(N), SC: O(1)
-       /* int[] result = new int[2];
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
-        }
-
-        return result;*/
-
-        //TC: O(N), SC: O(N)
-        Map<Integer, Integer> map = new HashMap<>();
+    public int[] twoSum(int[] nums, int t) {
+        Map<Integer, Integer> m = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
+            int diff = t - nums[i];
 
-        for (int i = 0; i < nums.length; i++) {
-            int tmp = target - nums[i];
-
-            if (map.containsKey(tmp) && map.get(tmp) != i) {
-                return new int[]{i, map.get(tmp)};
+            if (m.containsKey(diff)) {
+                return new int[]{i, m.get(diff)};
             }
+
+            m.put(nums[i], i);
         }
 
         return null;
