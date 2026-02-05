5175b6cf0e7ca2f6da4aa735bc91b0e462fffa56
Sergey Savrukhin <savra.sv@yandex.ru>
2026-02-06 00:19:20 +0300

    Valid Anagram

--- src/main/java/com/hvdbs/leetcode/solution/java/ValidAnagram.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/ValidAnagram.java
@@ -31,4 +31,26 @@ public class ValidAnagram {
 
         return true;
     }
+
+/*
+TC: O(N) SC: O(N + M)
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (--alphabet[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }*/
 }
