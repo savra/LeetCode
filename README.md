bc72cd660c118ea639a7519d4a24f8423a162a2a
Sergey Savrukhin <savra.sv@yandex.ru>
2026-02-13 22:28:32 +0300

    Two Sum II - Input Array Is Sorted

--- README.md
+++ README.md
@@ -361,7 +361,7 @@
 |Strictly Palindromic Number|https://leetcode.com/problems/strictly-palindromic-number/description/|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/StrictlyPalindromicNumber.java'>Strictly Palindromic Number</a>|$not specified$|$not specified$
 |Subrectangle Queries|https://leetcode.com/problems/subrectangle-queries/description/|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/SubrectangleQueries.java'>Subrectangle Queries</a>|$not specified$|$not specified$
 |Swap Nodes in Pairs|https://leetcode.com/problems/swap-nodes-in-pairs|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/SwapNodesInPairs.java'>Swap Nodes in Pairs</a>|$not specified$|$not specified$
-|Two Sum II - Input Array Is Sorted|https://leetcode.com/problems/two-sum-ii-input-array-is-sorted|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/TwoSumIIInputArrayIsSorted.java'>Two Sum II - Input Array Is Sorted</a>|$O(N^2)$|$O(1)$
+|Two Sum II - Input Array Is Sorted|https://leetcode.com/problems/two-sum-ii-input-array-is-sorted|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/TwoSumIIInputArrayIsSorted.java'>Two Sum II - Input Array Is Sorted</a>|$O(N * LogN)$|$O(1)$
 |Validate Binary Search Tree|https://leetcode.com/problems/validate-binary-search-tree|<a href='https://github.com/savra/LeetCode/blob/master/src/main/java/com/hvdbs/leetcode/solution/java/ValidateBinarySearchTree.java'>Validate Binary Search Tree</a>|$not specified$|$not specified$
 </details>
 <details>
--- src/main/java/com/hvdbs/leetcode/solution/java/TwoSumIIInputArrayIsSorted.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/TwoSumIIInputArrayIsSorted.java
@@ -1,14 +1,15 @@
 package com.hvdbs.leetcode.solution.java;
 
 import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
 import com.hvdbs.savra.statsgenerator.enums.Difficulty;
 
 @CodeInfo(
         difficulty = Difficulty.MEDIUM,
         name = "Two Sum II - Input Array Is Sorted",
         url = "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted",
-        timeComplexity = "O(N^2)",
-        spaceComplexity = "O(1)")
+        timeComplexity = Complexity.ConstantComplexity.LINEARITHMIC,
+        spaceComplexity = Complexity.ConstantComplexity.CONSTANT)
 public class TwoSumIIInputArrayIsSorted {
     //My Time Complexity: O(n^2)
     //My Space complexity: O(1)
@@ -27,5 +28,44 @@ public class TwoSumIIInputArrayIsSorted {
         }
 
         return result;
+    }
+
+    //My Time Complexity: O(N LogN)
+    //My Space complexity: O(1)
+    public int[] twoSum2(int[] numbers, int target) {
+        int i = 0, j = 1;
+
+        while (i < numbers.length && j < numbers.length) {
+            int subTarget = target - numbers[i];
+
+            int l = i + 1;
+            int r = numbers.length - 1;
+
+            if (r - l == 0) {
+                if (numbers[i] + numbers[r] == target) {
+                    return new int[]{i + 1, r + 1};
+                } else {
+                    return null;
+                }
+            }
+
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                int mVal = numbers[m];
+                if (mVal == subTarget) {
+                    return new int[]{i + 1, m + 1};
+                } else if (mVal < subTarget) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+
+            i++;
+            j++;
+
+        }
+
+        return null;
     }
 }
