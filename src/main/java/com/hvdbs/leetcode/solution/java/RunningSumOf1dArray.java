f2dc170d6497e4abbfbd5c5bca91f991f998eb46
Sergey Savrukhin <savra.sv@yandex.ru>
2026-05-24 09:52:23 +0300

    Running Sum of 1d Array

--- src/main/java/com/hvdbs/leetcode/solution/java/RunningSumOf1dArray.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/RunningSumOf1dArray.java
@@ -1,22 +1,24 @@
 package com.hvdbs.leetcode.solution.java;
 
 import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
 import com.hvdbs.savra.statsgenerator.enums.Difficulty;
 
 @CodeInfo(difficulty = Difficulty.EASY,
         name = "Running Sum of 1d Array",
-        url = "https://leetcode.com/problems/running-sum-of-1d-array/")
+        url = "https://leetcode.com/problems/running-sum-of-1d-array/",
+        timeComplexity = Complexity.ConstantComplexity.LINEAR,
+        spaceComplexity = Complexity.ConstantComplexity.CONSTANT
+)
 public class RunningSumOf1dArray {
     public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
+        int sum = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j <= i; j++) {
-
-                result[i] += nums[j];
-            }
+            sum += nums[i];
+            nums[i] = sum;
         }
 
-        return result;
+        return nums;
     }
 }
