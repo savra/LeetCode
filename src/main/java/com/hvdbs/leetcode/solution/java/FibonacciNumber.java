ae02f377e0204aca0397f40a6a6ee142726a6b6e
Sergey Savrukhin <savra.sv@yandex.ru>
2026-06-03 16:49:42 +0300

    Fibonacci Number

--- src/main/java/com/hvdbs/leetcode/solution/java/FibonacciNumber.java
+++ src/main/java/com/hvdbs/leetcode/solution/java/FibonacciNumber.java
@@ -1,11 +1,15 @@
 package com.hvdbs.leetcode.solution.java;
 
 import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
 import com.hvdbs.savra.statsgenerator.enums.Difficulty;
 
 @CodeInfo(difficulty = Difficulty.EASY,
         name = "Fibonacci Number",
-        url = "https://leetcode.com/problems/fibonacci-number/")
+        url = "https://leetcode.com/problems/fibonacci-number/",
+        spaceComplexity = Complexity.ConstantComplexity.LINEAR,
+        timeComplexity = Complexity.ConstantComplexity.CUBIC /*timeComplexity - 2^n*/
+)
 public class FibonacciNumber {
     public int fib(int n) {
         return n == 0 || n == 1 ? n : fib(n - 2) + fib(n - 1);
